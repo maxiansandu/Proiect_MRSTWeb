5a6eca6a8cd7dbac41132b95c63053e398c1a9d2
CertanGhenadie <ghenadie.certan@isa.utm.md>
2025-05-06 10:17:05 +0300

    Import Proiect_MRSTWeb-master/Proiect_MRSTWeb-master/RentaCarWEB/Solution1/lab_1_templates_sandu/Controllers/ContController.cs

---
+++ Proiect_MRSTWeb-master/Proiect_MRSTWeb-master/RentaCarWEB/Solution1/lab_1_templates_sandu/Controllers/ContController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eUseControl.Web.Controllers
+{
+    public class ContController : Controller
+    {
+        // GET: Cont
+        public ActionResult UserPage()
+        {
+            return View();
+        }
+
+        public ActionResult AdminPage()
+        {
+            return View();
+        }
+    }
+}
